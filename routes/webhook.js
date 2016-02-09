549fb6858998decea46b5fce495a772e418c5d3a
Ricardo Loo <ricardol@microsoft.com>
2016-02-09 09:38:47 -0800

    Added *webhooks* route

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Listen to Office 365 events. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
