f476c1357c84f5039fcf047105896977cd891756
Ricardo Loo <ricardol@microsoft.com>
2016-02-10 15:58:27 -0800

    Removed unused variable and wrapped the code in an anonymous function

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,10 @@
-var socket = io.connect('http://localhost:3001');
-// The token will only live in our database for 2 minutes
-var tokenLifetime = 120000;
+(function() {
+    var socket = io.connect('http://localhost:3001');
 
-// Handle the event_fired event.
-socket.on('event_received', function (data) {
-    var newListItem = document.createElement('li');
-    newListItem.innerText = 'Event id: ' + data;
-	document.getElementById('events').appendChild(newListItem);
-});
+    // Handle the event_received event.
+    socket.on('event_received', function (data) {
+        var newListItem = document.createElement('li');
+        newListItem.innerText = 'Event id: ' + data;
+        document.getElementById('events').appendChild(newListItem);
+    });    
+})();
