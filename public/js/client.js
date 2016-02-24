84149e17a918a9d3307d68acf76befd3d0d24c76
Ricardo Loo <ricardol@microsoft.com>
2016-02-24 13:03:35 -0800

    First working version of notification_received

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,17 @@
-(function() {
-    var socket = io.connect('http://localhost:3001');
+var socket = io.connect('http://localhost:3001');
 
-    // Handle the event_received event.
-    socket.on('event_received', function (data) {
-        var newListItem = document.createElement('li');
-        newListItem.innerText = 'Event id: ' + data;
-        document.getElementById('events').appendChild(newListItem);
-    });
-})();
+// Handle the event_received event.
+socket.on('notification_received', function (data) {
+    var newListItem = document.createElement('li');
+    newListItem.appendChild(document.createTextNode(data));
+    console.log('Notification received: ' + data);
+    document.getElementById('events').appendChild(newListItem);
+});
 
-function connect() {
-    window.location
+socket.on('create_room', function (data) {
+    console.log('Room created: ' + data);
+});
+
+function createSocketRoom (subscriptionId) {
+  socket.emit('create_room', subscriptionId);
 }
