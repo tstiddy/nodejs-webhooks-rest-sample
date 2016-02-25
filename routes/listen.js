cae71f43f928486ba03ec46c5c5d3c4394cbbe80
Ricardo Loo <ricardol@microsoft.com>
2016-02-25 12:23:11 -0800

    Taking the value of the valid clientState from the subscriptionConfiguration in constants.js

--- routes/listen.js
+++ routes/listen.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var io = require('../helpers/socketHelper.js');
 var http = require('http');
+var clientStateValueExpected = require('../constants').subscriptionConfiguration.clientState;
 
 /* Default webhooks route. */
 router.get('/', function(req, res) {
@@ -20,19 +21,21 @@ router.post('/', function(req, res) {
         // Send a status of 'Ok'
         status = 200;
     } else {
-        var clientStateValid = true;
+        var clientStatesValid = false;
         
         // First, validate all the clientState values in array
         for(var i = 0; i < req.body.value.length; i++) {
-            if(req.body.value[i].clientState !== 'cLIENTsTATEfORvALIDATION') {
+            if(req.body.value[i].clientState !== clientStateValueExpected) {
                 // If just one clientState is invalid, we discard the whole batch
-                clientStateValid = false;
+                clientStatesValid = false;
                 break;
+            } else {
+                clientStatesValid = true;
             }
         }
         
         // If all the clientStates are valid, then wen notify the socket(s)
-        if (clientStateValid) {
+        if (clientStatesValid) {
             for(var i = 0; i < req.body.value.length; i++) {
                 io.to(req.body.value[i].subscriptionId).emit('notification_received', req.body.value[i].resource);
             }
