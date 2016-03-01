f817703db2ea926f6367d0f0d0f0bd139a136486
Ricardo Loo <ricardol@microsoft.com>
2016-03-01 13:26:20 -0800

    Removed the subscription ID field from the notification list

--- public/js/client.js
+++ public/js/client.js
@@ -5,10 +5,9 @@ socket.on('notification_received', function (notification) {
     var newListItem = document.createElement('li');
 
     var changeTypeText = '<b>Change type:</b> ' + notification.changeType;
-    var subscriptionIdText = '<b>Subscription ID:</b> ' + notification.subscriptionId;
     var resourceText = '<b>Resource:</b> ' + notification.resource;
 
-    newListItem.innerHTML = changeTypeText + '<br />' + subscriptionIdText  + '<br />' + resourceText;
+    newListItem.innerHTML = changeTypeText + '<br />' + resourceText;
     console.log('Notification received: ' + JSON.stringify(notification));
     document.getElementById('events').appendChild(newListItem);
 });
