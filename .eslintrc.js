271564595608f7ba69ebae66f7790265b44a528e
Ricardo Loo <ricardol@microsoft.com>
2016-03-09 11:06:19 -0800

    Initial configuration of ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+  "extends": "airbnb",
+  "rules":{
+    "prefer-template": 0,
+    "comma-dangle": 0,
+    "func-names": 0,
+    "prefer-arrow-callback": 0
+  }
+};
