6f5727ca43ffd9dbaa5241d31d955222f60930de
Ricardo Loo <ricardol@microsoft.com>
2016-03-10 17:14:50 -0800

    Updated test text and placeholders in configuration file

--- tests/confTest.js
+++ tests/confTest.js
@@ -9,7 +9,11 @@ describe('ADAL', function () { // eslint-disable-line no-undef
         isADALConfigured(conf.adalConfiguration),
         '\nRegister client ID and client secret in file constants.js.\n' +
         'You don\'t have them? Get them by using the Office 365 app registration tool\n' +
-        'http://dev.office.com/app-registration'
+        'http://dev.office.com/app-registration\n' +
+        'App type: Web App\n' +
+        'Sign on URL: http://localhost:3000\n' +
+        'Redirect URI: http://localhost:3000/callback\n' +
+        'App permissions: Mail.Read'
       );
     }
   );
@@ -22,7 +26,8 @@ describe('NotificationURL', function () { // eslint-disable-line no-undef
       '\nConfigure the notification URL in file constants.js.\n' +
       'Install ngrok from https://ngrok.com/download and run\n' +
       '\tngrok http 3000\n' +
-      'Use the https public URL in the notificationUrl property.'
+      'Copy the NGROK_ID in https://NGROK_ID.ngrok.io from the output\n' +
+      'of the command above to the notificationUrl property.'
     );
   });
 });
@@ -47,7 +52,7 @@ function isSubscriptionConfigured(configuration) {
     typeof(configuration.notificationUrl) !== 'undefined' &&
     configuration.notificationUrl !== null &&
     configuration.notificationUrl !== '' &&
-    configuration.notificationUrl !== 'ENTER_YOUR_NOTIFICATION_URL';
+    configuration.notificationUrl.indexOf('NGROK_ID') === -1;
 
   return notificationURLConfigured;
 }
