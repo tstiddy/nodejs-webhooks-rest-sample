c16b47ba9febb11eb5623e31ee03a98b3188c886
Vincent Biret <vibiret@microsoft.com>
2020-04-23 16:40:25 -0400

    - adds private key loading from pem file - adds symetric key decryptiong - adds payload decryption

--- helpers/certificateHelper.js
+++ helpers/certificateHelper.js
@@ -2,6 +2,7 @@ import pem from 'pem';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import crypto from 'crypto';
 
 function ensureOpenSsl() {
   if (os.platform() === 'win32') {
@@ -25,4 +26,25 @@ export function getSerializedCertificate(certPath) {
   const pfx = fs.readFileSync(path.join(__dirname, certPath));
   const pfxAsString = pfx.toString().replace(/(\r\n|\n|\r|-|BEGIN|END|CERTIFICATE|\s)/gm, '');
   return pfxAsString;
+}
+
+export function getPrivateKey(keyPath) {
+  const privateKey = fs.readFileSync(path.join(__dirname, keyPath), 'utf8');
+  return privateKey;
+}
+
+export function decryptSymetricKey(base64encodedKey, keyPath) {
+  const asymetricPrivateKey = getPrivateKey(keyPath);
+  const decodedKey = Buffer.from(base64encodedKey, 'base64');
+  const decryptedSymetricKey = crypto.privateDecrypt(asymetricPrivateKey, decodedKey);
+  return decryptedSymetricKey;
+}
+
+export function decryptPayload(base64encodedPayload, decryptedSymetricKey) {
+  const iv = Buffer.alloc(16, 0);
+  decryptedSymetricKey.copy(iv, 0, 0, 16);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', decryptedSymetricKey, iv);
+  let decryptedPayload = decipher.update(base64encodedPayload, 'base64', 'utf8');
+  decryptedPayload += decipher.final('utf8');
+  return decryptedPayload;
 }
