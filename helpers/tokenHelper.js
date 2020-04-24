c6630c5eaf49701acfb7155433feafb9f4ca28c7
Vincent Biret <vincentbiret@hotmail.com>
2020-04-24 09:11:11 -0400

    - adds tokens validation

---
+++ helpers/tokenHelper.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import jkwsClient from 'jwks-rsa';
+
+const client = jkwsClient({
+  jwksUri: 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
+});
+
+export function getKey(header, callback) {
+  client.getSigningKey(header.kid, (err, key) => {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+export function isTokenValid(token, appId, tenantId) {
+  return new Promise((resolve) => {
+    const options = {
+      audience: [appId],
+      issuer: [`https://sts.windows.net/${tenantId}/`]
+    };
+    jwt.verify(token, getKey, options, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
