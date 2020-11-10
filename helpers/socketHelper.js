d5f2b70e199326edf577342832b86d1619122838
Vincent Biret <vibiret@microsoft.com>
2020-11-10 13:08:11 -0500

    - adds mandatory explicit CORS configuration for socket.io

--- helpers/socketHelper.js
+++ helpers/socketHelper.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import http from 'http';
 import io from 'socket.io';
+import { subscriptionConfiguration, msalConfiguration } from '../constants';
 
 const socketServer = http.createServer(express);
-export const ioServer = io(socketServer);
+export const ioServer = io(socketServer, {
+  cors: {
+    origin: [
+      msalConfiguration.redirectUri.substring(0, msalConfiguration.redirectUri.lastIndexOf('/')),
+      subscriptionConfiguration.notificationUrl.substring(0, subscriptionConfiguration.notificationUrl.lastIndexOf('/'))],
+    methods: ['GET', 'POST']
+  }
+});
 
 socketServer.listen(3001);
 
